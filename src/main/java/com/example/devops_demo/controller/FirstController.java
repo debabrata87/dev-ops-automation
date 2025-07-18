65088406563d4a57a7978747a68189128f0a301b
debabrata87 <debabrata_fly@yahoo.co.in>
2025-07-18 22:17:49 +0100

    devops demo calling endpoint for testing other microservice calls

--- src/main/java/com/example/devops_demo/controller/FirstController.java
+++ src/main/java/com/example/devops_demo/controller/FirstController.java
@@ -1,7 +1,10 @@
 
 package com.example.devops_demo.controller;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +15,15 @@ public class FirstController {
 		return " Hello World , My First Demo for Dev Ops";
 	}
 
+	
+	@GetMapping("/greetv1/{name}")
+	public String welcomeV2(@PathVariable("name")  String name) {
+		System.out.println(System.getProperty("java.version"));
+		return " App Name V1: DevOpsDemo. User Name:" +name +" , Time "+ new Date();
+	}
+	@GetMapping("/greetv2/{name}")
+	public String welcomeV3(@PathVariable("name")  String name) {
+		System.out.println(System.getProperty("java.version"));
+		return " App Name V2: DevOpsDemo. User Name:" +name +" , Time "+ new Date();
+	}
 }

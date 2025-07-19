2f5a4a6301b756cd2efcd0e72f58f72e36c2a16a
debabrata87 <debabrata_fly@yahoo.co.in>
2025-07-19 23:56:22 +0100

    With Eureka Clientr

--- src/main/java/com/example/devops_demo/DevopsDemoApplication.java
+++ src/main/java/com/example/devops_demo/DevopsDemoApplication.java
@@ -2,10 +2,12 @@ package com.example.devops_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
 
 
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = {
 	    org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration.class,
 	    org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration.class
